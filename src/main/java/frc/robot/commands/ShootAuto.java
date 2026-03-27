c0c82963e435bddf7b24419b1740437b8e79e089
hnallagatla <hemanthnallagatla@gmail.com>
2026-03-26 21:32:11 -0400

    changes

--- src/main/java/frc/robot/commands/ShootAuto.java
+++ src/main/java/frc/robot/commands/ShootAuto.java
@@ -12,26 +12,20 @@ public class ShootAuto {
   // public static Command shoot(Turret turret, Indexer indexer, VisionSubsystem
   // vision) {
   // return Commands.sequence(
-  // // Step 1: Start flywheel
   // Commands.runOnce(() -> turret.setFlywheelPercent(0.7)),
 
-  // // Step 2: Raise hood while flywheel spins up
   // Commands.parallel(
   // Commands.runOnce(() -> turret.setHoodPercent(0.15)),
   // Commands.waitSeconds(0.5) // Wait for hood to raise
   // ),
 
-  // // Step 3: Wait for flywheel to spin up
   // Commands.waitSeconds(1.5),
 
-  // // Step 4: Start feeding
   // Commands.runOnce(() -> indexer.setPercent(-0.6)),
 
-  // // Step 5: Feed for duration
   // Commands.waitSeconds(2.0), // Adjust this time based on how long feeding
   // takes
 
-  // // Step 6: Stop everything
   // Commands.runOnce(() -> {
   // indexer.stop();
   // turret.stopFlywheel();
@@ -40,24 +34,20 @@ public class ShootAuto {
   // );
   // }
 
-  public static Command shoot(Turret turret, Indexer indexer, Intake intake, VisionSubsystem vision) {
+  public static Command shoot(Turret turret, Indexer indexer, Intake intake,
+      VisionSubsystem vision) {
     return Commands.sequence(
-        // Step 1: Start flywheel
 
         Commands.runOnce(() -> intake.setExtensionPosition(1.3)),
-        Commands.parallel(
-          Commands.runOnce(() -> turret.setFlywheelPercent(0.5)),
-          Commands.waitSeconds(1) // Wait for hood to raise
-        ),
-        Commands.parallel(
-            Commands.run(() -> turret.setFlywheelPercent(0.45), turret),
-            // Commands.run(()-> turret.setFlywheelPercent(0.7), turret),
+        Commands.parallel(Commands.runOnce(() -> turret.setFlywheelPercent(0.5)),
+            Commands.waitSeconds(1)),
+        Commands.parallel(Commands.run(() -> turret.setFlywheelPercent(0.45), turret),
+            // Commands.runOnce(() -> turret.setHoodPercent(0.15)),
             Commands.run(() -> indexer.setPercent(-0.4), indexer)).withTimeout(10),
-        // Step 6: Stop everything
         Commands.runOnce(() -> {
           indexer.stop();
           turret.stopFlywheel();
-          turret.stopHood(); // Use brake instead of stop
+          turret.stopHood();
         }));
   }
 }
